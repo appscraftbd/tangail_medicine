a68180f1500d358efdf525b5db26ff8ba33b67ea
mdnah <appscraftbd@gmail.com>
2024-01-07 15:23:55 +0600

    v 0.1

---
+++ app/src/main/java/com/example/tangailmedicine/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.tangailmedicine;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+    }
+}
